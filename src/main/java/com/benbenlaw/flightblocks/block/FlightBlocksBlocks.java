1d6dd8797c0a42c38ab90407f805ed41809ae4d2
benbenlaw <benlawrenceben@hotmail.co.uk>
2025-07-01 15:11:37 +0100

    1.21.6

--- src/main/java/com/benbenlaw/flightblocks/block/FlightBlocksBlocks.java
+++ src/main/java/com/benbenlaw/flightblocks/block/FlightBlocksBlocks.java
@@ -13,6 +13,7 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class FlightBlocksBlocks {
@@ -22,5 +23,25 @@ public class FlightBlocksBlocks {
 
     public static final DeferredBlock<Block> FLIGHT_BLOCK = BLOCKS.registerBlock("flight_block",
             FlightBlock::new, BlockBehaviour.Properties.ofFullCopy(Blocks.POLISHED_ANDESITE));
+
+
+    /* Below is the best practive move to this when understanding how tool tips work in Minecraft.
+
+
+    public static final DeferredBlock<Block> FLIGHT_BLOCK = registerBlock("flight_block",
+            properties -> new FlightBlock(BlockBehaviour.Properties.ofFullCopy(Blocks.POLISHED_ANDESITE).noCollission()));
+
+
+    private static <T extends Block> DeferredBlock<T> registerBlock(String name, Function<BlockBehaviour.Properties, T> function) {
+        DeferredBlock<T> toReturn = BLOCKS.registerBlock(name, function);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
+        FlightBlocksItems.ITEMS.registerItem(name, (properties) -> new BlockItem(block.get(), properties.useBlockDescriptionPrefix()));
+    }
+
+     */
 
 }
--- src/main/java/com/benbenlaw/flightblocks/block/entity/FlightBlockEntities.java
+++ src/main/java/com/benbenlaw/flightblocks/block/entity/FlightBlockEntities.java
@@ -21,8 +21,8 @@ public class FlightBlockEntities {
             DeferredRegister.create(BuiltInRegistries.BLOCK_ENTITY_TYPE, FlightBlocks.MOD_ID);
 
     public static final Supplier<BlockEntityType<FlightBlockEntity>> FLIGHT_BLOCK_ENTITY =
-            BLOCK_ENTITIES.register("flight_block_entity",
-                    () -> new BlockEntityType<>(FlightBlockEntity::new,FlightBlocksBlocks.FLIGHT_BLOCK.get()));
+            BLOCK_ENTITIES.register("flight_block_entity", () ->
+                    new BlockEntityType<>(FlightBlockEntity::new,FlightBlocksBlocks.FLIGHT_BLOCK.get()));
 
 
 }
